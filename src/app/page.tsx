d293600a1b19b0bfb7a83a2227576a802e8e42a5
Harsh_Pachauri <harshpachauri205@gmail.com>
2024-12-02 12:41:47 +0530

    Add Featured Cards, Why Choose Us, Upcoming Webinars, Instructors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { main } from "framer-motion/client";
 import Image from "next/image";
 
@@ -7,6 +12,11 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       {/* <h1 className="text-2xl -text-center">Hey Harsh!!</h1> */}
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
     </main>
   );
 }
