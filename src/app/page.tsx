8cd188e939bfbef0bf54620e65bdd3f0df7eebeb
Harsh_Pachauri <harshpachauri205@gmail.com>
2024-12-02 18:04:52 +0530

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
@@ -9,7 +10,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+    <main className="min-h-screen bg-black/[0.96] antialiased ">
       {/* <h1 className="text-2xl -text-center">Hey Harsh!!</h1> */}
       <HeroSection/>
       <FeaturedCourses/>
@@ -17,6 +18,7 @@ export default function Home() {
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
       <Instructors/>
+      <Footer/>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,40 @@
 'use client'
-//whenever there is ui manipulation
+// whenever there is UI manipulation
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils/cn";
 import Link from "next/link";
 
-
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+  
   return (
     <div
-    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link className="flex space-x-4" href={'/'}>
+          <Link className="flex space-x-4" href="/">
         <MenuItem setActive={setActive} active={active} item="Home">
-        
         </MenuItem>
+          </Link>
+
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/courses">All courses</HoveredLink>
-            <HoveredLink href="/courses">Basic Music courses</HoveredLink>
-            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-            <HoveredLink href="/courses">song Writing</HoveredLink>
-            <HoveredLink href="/courses">Music Production</HoveredLink>
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses/basic">Basic Music Courses</HoveredLink>
+            <HoveredLink href="/courses/advanced">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses/songwriting">Song Writing</HoveredLink>
+            <HoveredLink href="/courses/production">Music Production</HoveredLink>
           </div>
         </MenuItem>
+
+          <Link href="/contact">
         <MenuItem setActive={setActive} active={active} item="Contact Us">
-        
         </MenuItem>
-        </Link>
+          </Link>
       </Menu>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
